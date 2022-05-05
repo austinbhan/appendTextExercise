3c8a16dabdf0748bb1a9696888481103b7d78ae2
austinbhan <austinbhan@outlook.com>
2022-05-05 16:04:51 -0700

    Functioning Example

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // import functions and grab DOM elements
+const textInput = document.getElementById('text-input');
+const inputButton = document.getElementById('input-button');
+const pushedHTML = document.getElementById('pushedHTML');
 
 // let state
 
@@ -6,3 +9,24 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+let stuff = [];
+
+inputButton.addEventListener('click', () => {
+    let textValue = textInput.value; 
+    stuff.push(textValue);
+    displayStuff();
+});
+
+function displayStuff() {
+  // will clear out whateve'rs in the list 
+  // loop through each item in stuff
+  // create an li w/item
+  // append li to the list
+    pushedHTML.textContent = '';
+    for (let thing of stuff) {
+        const li = document.createElement('li');
+        li.textContent = thing;
+        pushedHTML.append(li);
+    }
+}
